c318b81773b670eef97d7e859ed7b85a6c6c8a0b
jim berry <jim@bigfattail.com>
2019-11-16 08:04:04 -0500

    init

---
+++ P2pNetTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Newtonsoft.Json;
+using P2pNet;
+
+namespace Tests
+{
+
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void NewTest()
+    {
+
+    }
+
+   
+}
